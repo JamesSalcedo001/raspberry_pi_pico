4b3eed464bcd4a9aa24b1d52031254c74c7cce99
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-16 00:36:24 -0600

    Reconfigured connection

--- connect/main.py
+++ connect/main.py
@@ -1,15 +1,9 @@
 # connect to wifi network
 import network
 import time
-import os
-from dotenv import load_dotenv
+from secrets import SSID, PASSWORD
 
-# Load environment variables from .env file
-load_dotenv()
 
-# Retrieve Wi-Fi credentials from environment variables
-SSID = os.getenv("SSID")
-PASSWORD = os.getenv("PASSWORD")
 
 # create an object to create connection from code to Pico wireless chip
 wlan = network.WLAN(network.STA_IF)
@@ -31,6 +25,5 @@ while max_wait > 0:
 
 if wlan.isconnected():
     print('Connected successfully!')
-    print('Network configuration:', wlan.ifconfig())
 else:
     print('Failed to connect.')
