9501ca8b250c7ed9e1044d1eb9e35224e26415e5
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-16 00:32:25 -0600

    test

--- connect/main.py
+++ connect/main.py
@@ -1,34 +1,36 @@
 # connect to wifi network
 import network
-
-# add a pause to code
 import time
-
 import os
-
 from dotenv import load_dotenv
 
-
-# load environment variables
+# Load environment variables from .env file
 load_dotenv()
 
-# retrieve wifi credentials from .env
-
+# Retrieve Wi-Fi credentials from environment variables
 SSID = os.getenv("SSID")
 PASSWORD = os.getenv("PASSWORD")
 
-
 # create an object to create connection from code to Pico wireless chip
-
-
-# use this to connect and check wifi connection
 wlan = network.WLAN(network.STA_IF)
 
 # turn on raspberry pi pico w's wifi
 wlan.active(True)
 
-
 # connect to router using env variables
 wlan.connect(SSID, PASSWORD)
 
-print(wlan.isconnected())
+# Check and print connection status
+max_wait = 15
+while max_wait > 0:
+    if wlan.isconnected():
+        break
+    print('waiting for connection...')
+    time.sleep(1)
+    max_wait -= 1
+
+if wlan.isconnected():
+    print('Connected successfully!')
+    print('Network configuration:', wlan.ifconfig())
+else:
+    print('Failed to connect.')
