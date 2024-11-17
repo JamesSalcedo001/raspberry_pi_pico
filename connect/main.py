a85a5e87804f2bff8f367359935f64514d699bf9
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-11-16 23:50:56 -0600

    Add comments to code pt 2

--- connect/main.py
+++ connect/main.py
@@ -33,7 +33,11 @@ else:
 # create astronaut object and use urequests to get info in JSON format
 astronauts = urequests.get("http://api.open-notify.org/astros.json").json()
 
+# create object, number which will open astronauts object and look for the key "number", the value linked to the key is then stored in the number object
 number = astronauts["number"]
 
+# create loop that iterates for the number of people on the international space station
 for i in range(number):
+    # print name of each astronaut on the International space station using a series of keys that target the specific data
     print(astronauts['people'][i]['name'])
+    # i increments every time the loop repeats and selects each person from the list of embedded data, then use "name" key to access the name values of each
